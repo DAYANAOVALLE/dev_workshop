746667bc561ff56882802e18b3dc0218337240c6
dayana <dcovalle72@ucatolica.edu.co>
2025-03-18 16:20:34 -0500

    ultimas modificaciones para los test

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -2,40 +2,29 @@ class Logica:
     
     def AND(self, a, b):
         return a and b
-        pass
     
     def OR(self, a, b):
         return a or b 
-
-        pass
     
     def NOT(self, a):
         return not a 
-        pass
     
     def XOR(self, a, b):
         return a!=b
     
-        pass
-    
     def NAND(self, a, b):
         return not (a and b)
-        pass
     
     def NOR(self, a, b):
         return not (a or b)
-        pass
     
     def XNOR(self, a, b):
         return a == b
-        pass
     
     def implicacion(self, a, b):
         return not a or b
-        pass
     
     def bi_implicacion(self, a, b):
         return a == b
-        pass
     
     
